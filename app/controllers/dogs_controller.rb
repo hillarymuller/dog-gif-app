4e12a3cea7717f760398c6b64a7b20dc425b0316
Hillary Muller <hillaryaward@gmail.com>
2023-01-09 16:27:58 -0600

    index, show, update in dogs controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,27 @@
 class DogsController < ApplicationController
+    skip_before_action :authorize
+    before_action :find_dog, only: [:show, :update]
+    def index
+        dogs = Dog.all
+        render json: dogs
+    end
+
+    def show
+        render json: @dog
+    end
+    def update
+        if @current_user.dogs.include?(@dog) || @current_user.household.users.filter { |u| u.id == @dog.user_id }
+            @dog&.update!(dog_params)
+            render json: @dog
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  
+    private
+    def dog_params
+        params.permit(:id, :user_id, :hunger, :thirst, :happiness, :energy, :potty)
+    end
+    def find_dog
+        @dog = Dog.find(params[:id])
+    end
 end

5e397ea84fd372e258d5a066b709e2ea20b2d54a
Hillary Muller <hillaryaward@gmail.com>
2023-01-09 16:33:04 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
   
   resources :dog_treats
   resources :treats
-  resources :dogs
-  resources :users
+  resources :dogs, except: :destroy
   resources :households
+  # user routes
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+
+  # sessions routes
+  post "/signin", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }

cb2246b1fa6ffc9e7fc28f776ec342c57addff64
Hillary Muller <hillaryaward@gmail.com>
2023-01-09 16:08:02 -0600

    generate resources for users & dogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  resources :dogs
+  resources :users
   resources :households
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!

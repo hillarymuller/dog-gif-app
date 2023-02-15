75abaeedc6e4a656c0ec182a8a6d4ac466c5f6af
Hillary Muller <hillaryaward@gmail.com>
2023-02-15 15:07:52 -0500

    remove reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-rails db:reset 
 
 Treat.create([{name: "pup pop"}, {name: "peanut butter"}, {name: "sweet potato"}, 
 {name: "cheese"}, {name: "marshmallow"}, {name: "salmon jerky"}])

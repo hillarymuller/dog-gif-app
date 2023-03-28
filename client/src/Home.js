c46b632051a9afd5e4d77790f96478548d86e5d7
Hillary Muller <hillaryaward@gmail.com>
2023-03-28 14:12:12 -0500

    change home page instructions

--- client/src/Home.js
+++ client/src/Home.js
@@ -11,7 +11,7 @@ function Home() {
             <h3>Feeling like snoozin' throughout your workday? Take a little break to virtually adopt & take care of cute dogs!</h3>
             <span>Sign up & log in to get started. Create and connect your household to your account so you can take care of family members' dogs too!
                 You can adopt existing dogs or create new ones with your own gifs! Try to keep the dogs' potty, hunger, energy, & thirst scores from reaching 10 while maintaining a high level of happiness!
-                The dogs' needs will update hourly, so check back a few times each day to take care of your dogs.</span>
+                The dogs' needs will update every 5 minutes, so check back a few times each day to take care of your dogs.</span>
         </div>
     )
 };
